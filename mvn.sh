4cbcd8fa36b5696a85710cc50d964558d7313ec9
sunxiaoou <sun_xiaoou@qq.com>
2017-10-27 15:28:29 +0800

    add timeout for managed container

--- mvn.sh
+++ mvn.sh
@@ -17,5 +17,5 @@
 
 # mvn -P arquillian-weld-ee-embedded clean test
 # mvn -P arquillian-glassfish-embedded clean test
-mvn -P arquillian-wls-remote clean test
-# mvn -P arquillian-wls-managed clean test
+# mvn -P arquillian-wls-remote clean test
+mvn -P arquillian-wls-managed clean test
